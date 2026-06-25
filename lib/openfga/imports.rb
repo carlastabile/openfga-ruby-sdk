cbf57d29d19523cc72eab35c2e07203a2b5df531
Steven Hobbs <steve.hobbs@hey.com>
2026-06-25 17:03:43 +0100

    chore: move api_executor models into client/models

--- lib/openfga/imports.rb
+++ lib/openfga/imports.rb
@@ -12,9 +12,9 @@ require 'openfga/configuration'
 require 'openfga/client/client_errors'
 require 'openfga/client/openfga_client'
 
-# Models
-require 'openfga/models/api_executor_request'
-require 'openfga/models/api_executor_response'
+# Client models
+require 'openfga/client/models/api_executor_request'
+require 'openfga/client/models/api_executor_response'
 
 # Token management
 require 'openfga/token_manager/token_manager'
